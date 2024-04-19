75a003cf0cce34763f830f4ed49fa6b4df400dd4
Fabian Gans <fgans@bgc-jena.mpg.de>
2024-04-19 10:28:48 +0200

    initial commit

--- src/DateTimes64.jl
+++ src/DateTimes64.jl
@@ -1,5 +1,42 @@
 module DateTimes64
+export DateTime64
+using Dates: Period, TimeType, Date, DateTime, Dates
+struct DateTime64{P} <: TimeType
+    i::Int64
+end
+DateTime64{P}(d::TimeType) where P = convert(DateTime64{P},d)
+DateTime64(d::TimeType) = convert(DateTime64{Dates.Nanosecond},d)
+Base.convert(::Type{Date},t::DateTime64{P}) where P = Date(1970)+P(t.i)
+Base.convert(::Type{DateTime},t::DateTime64{P}) where P = DateTime(1970)+P(t.i)
+Base.show(io::IO,t::DateTime64{P}) where P = print(io,"DateTime64[",P,"]: ",string(DateTime(t)))
+Base.isless(x::DateTime64{P}, y::DateTime64{P}) where P = isless(x.i, y.i)
+import Base: ==
+==(x::DateTime64{P}, y::DateTime64{P}) where P = x.i == y.i
+const strpairs = (Dates.Year => "Y", Dates.Month => "M", Dates.Week => "W", Dates.Day=>"D", 
+    Dates.Hour => "h", Dates.Minute => "m", Dates.Second=>"s", Dates.Millisecond =>"ms",
+    Dates.Microsecond => "us", Dates.Nanosecond => "ns")
 
-# Write your package code here.
+for (jlt,s) in strpairs
+    @eval pydatetime_string(d::Type{<:DateTime64{$jlt}}) = string("<M8[",$s,"]")
+end
+pydatetime_string(x::DateTime64{P}) where P = pydatetime_string(DateTime64{P}) 
+
+function datetime_from_pystring(s)
+    m = match(s,r"<M8\[(\w+)\]")
+    if m === nothing
+        throw(ArgumentError("Could not parse datetime description"))
+    else
+        s = m.match
+        i = findfirst(i->last(i)==s,strpairs)
+        i === nothing && throw(ArgumentError("Could not parse datetime description"))
+        DateTime64{first(strpairs[i])}
+    end
+end
+
+Base.convert(::Type{DateTime64{P}}, t::Date) where P = DateTime64{P}(Dates.value(P(t-Date(1970))))
+Base.convert(::Type{DateTime64{P}}, t::DateTime) where P = DateTime64{P}(Dates.value(P(t-DateTime(1970))))
+Base.convert(::Type{DateTime64{P}}, t::DateTime64{Q}) where {P,Q} = DateTime64{P}(Dates.value(P(Q(t.i))))
+Base.zero(t::Union{DateTime64, Type{<:DateTime64}}) = t(0)
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,39 @@
 using DateTimes64
 using Test
+using Dates
 
 @testset "DateTimes64.jl" begin
-    # Write your tests here.
+
+#Create some datetime64s in Python
+#Originally the byte vector was created like this. Here we simply paste the result to avoid the
+#Python and numpy test dependency
+# parray = np.array(["2007-07-13", "2006-01-13", "2010-08-13"], dtype="datetime64")
+# jlbytes = pyconvert(Array,parray.tobytes())
+
+jlbytes = UInt8[0x8b, 0x35, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x69, 0x33, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf2, 0x39, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+dt64 = reinterpret(DateTime64{Dates.Day},jlbytes)
+@test Date.(dt64) == [Date(2007,07,13),Date(2006,1,13),Date(2010,8,13)]
+@test DateTime.(dt64) == [DateTime(2007,07,13),DateTime(2006,1,13),DateTime(2010,8,13)]
+@test dt64[1] > dt64[2]
+@test dt64[1] == dt64[1]
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Year}) == "<M8[Y]"
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Month}) == "<M8[M]"
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Week}) == "<M8[W]"
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Day}) == "<M8[D]"
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Hour}) == "<M8[h]"
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Minute}) == "<M8[m]"
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Second}) == "<M8[s]"
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Millisecond}) == "<M8[ms]"
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Microsecond}) == "<M8[us]"
+@test DateTimes64.pydatetime_string(DateTime64{Dates.Nanosecond}) == "<M8[ns]"
+@test DateTimes64.pydatetime_string(dt64[1]) == "<M8[D]"
+@test Date(zero(DateTime64{Dates.Day})) == Date(1970)
+@test DateTime64{Dates.Day}(Date(1970,1,5)) == DateTime64{Day}(4)
+@test DateTime64(Date(1970,1,5)) == DateTime64{Nanosecond}(345600000000000)
+@test DateTime64{Dates.Day}(DateTime(1970,1,5)) == DateTime64{Day}(4)
+@test DateTime64(DateTime(1970,1,5)) == DateTime64{Nanosecond}(345600000000000)
+@test DateTime64{Dates.Day}(DateTime64{Dates.Hour}(48)) == DateTime64{Day}(2)
+buf = IOBuffer()
+show(buf,dt64[1])
+@test String(take!(buf)) == "DateTime64[Day]: 2007-07-13T00:00:00"
 end
